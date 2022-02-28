f8a41ada030a3bc15af8ec416563e6e1236c3e9f
Knutrombonar <knutrl99@gmail.com>
2022-02-28 12:06:43 +0100

    Entered the numerical parameter values

---
+++ Parameter.m
@@ -0,0 +1,24 @@
+% Inertias
+I_yy  = 4370*10^7
+m     = 1993*10^3
+% Buoyancy Force
+B     = 1955*10^7
+% Added Mass
+Z_dw  = -1655*10^6
+Z_dq  = -1.574*10^5
+M_dw  = -4.302*10^5
+M_dq  = -4.062*10^8
+% Damping Coefficients
+Z_w   = -4.746*10^4
+Z_q   = -5024*10^5
+M_w   = 5484*10^5
+M_q   = -2430*10^7
+% Center of Buoyancy
+Z_B   = 0.1815
+% Control Input Coefficients
+Z_dels= -6850*10^3
+M_dels= -1992*10^5
+% Velocity
+u = 8
+
+save param
