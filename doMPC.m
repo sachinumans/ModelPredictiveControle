fde220eb7c735a353415ba6c0952d5f72eb625b3
sachinumans <sachinumans@gmail.com>
2022-03-28 09:00:33 +0200

    awd

--- doMPC.m
+++ doMPC.m
@@ -6,7 +6,7 @@ load System.mat
 load cstrMat.mat
 load CostMat.mat
 
-x0 = [0.1;0;0;0.4];
+x0 = [0;0;0.2;0.2];
 
 [T,S] = predmodgen(sys,dim,x0);
 
@@ -14,15 +14,18 @@ if any(cstr.X_cstr1*x0 >= cstr.X_cstr_b1)
     error("Initial state is outside of state set")
 end
 
-
+yRef = [0; 0.1];
+[xRef,uRef] = getRef(yRef, sys, cstr)
+uRefN = repmat(uRef, dim.N, 1);
 
 %%
 n = 20; x = [x0, zeros(4,n-1)]; u = zeros(2, n); % init
 if n < dim.N; error("Simulation time too short"); end
-u(:,1) = MPCgetInput(T, S, cstr, R_scld, Q_scld, P, dim, x0);
+u(:,1) = MPCgetInput(T, S, cstr, R_scld, Q_scld, P, dim, xRef,uRefN, x0);
 for t = 2:n
     x(:,t) = sys.A*x(:, t-1) + sys.B*u(:, t-1);
-    u(:,t) = MPCgetInput(T, S, cstr, R_scld, Q_scld, P, dim, x(:,t));
+    [T,S] = predmodgen(sys,dim,x(:,t));
+    u(:,t) = MPCgetInput(T, S, cstr, R_scld, Q_scld, P, dim, xRef,uRefN, x(:,t));
 end
 
 %%
